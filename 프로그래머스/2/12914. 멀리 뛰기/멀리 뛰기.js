abf3abe3de611e4ce4471d407761e143c2819974
sngmin <134246428+KingsMinn@users.noreply.github.com>
2025-11-03 17:33:55 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,10 +1,11 @@
 function solution(n) {
-    let dp = [0, 1, 2];
+    let dp = Array(n).fill(0);
+    [dp[1], dp[2]] = [1, 2];
     const DIVIDER = 1234567;
     if (n < dp.length) return dp[n];
     
-    for (let num = dp.length; num <= n; num++)
-        dp[dp.length] = (dp[dp.length - 2] + dp[dp.length - 1]) % DIVIDER;
+    for (let i = 3; i <= n; i++)
+        dp[i] = (dp[i-2] + dp[i-1]) % DIVIDER;
     
-    return dp[dp.length - 1] % DIVIDER
+    return dp[dp.length - 1]
 }
