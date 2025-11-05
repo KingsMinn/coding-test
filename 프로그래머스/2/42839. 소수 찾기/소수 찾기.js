da1b6590f2be62d0c79e85f02a4369834c065980
sngmin <134246428+KingsMinn@users.noreply.github.com>
2025-11-05 22:19:19 +0900

    [level 2] Title: 소수 찾기, Time: 245.73 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -0,0 +1,28 @@
+function isPrimeNumber(num) {
+    num = Number(num);
+    if (num < 2) return false;
+    for (let i = 2; i < num; i++) {
+        if (num%i === 0) return false;
+    }
+    return true;
+}
+
+function solution(numbers) {
+    const set = new Set();
+    const visited = Array(numbers.length).fill(false);
+    
+    function dfs(currentNum = "") {
+        for (let i = 0; i < numbers.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                const newNum = currentNum + numbers[i];
+                if (isPrimeNumber(newNum)) set.add(Number(newNum));
+                dfs(newNum);
+                visited[i] = false;
+            }
+        }
+    }
+    dfs();
+    
+    return set.size
+}
