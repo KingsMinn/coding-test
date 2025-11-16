4d00ba4240641ebcd767935b35128c33d2f16e03
sngmin <134246428+sngminn@users.noreply.github.com>
2025-11-16 22:42:35 +0900

    [level 3] Title: 정수 삼각형, Time: 9.20 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,10 +1,4 @@
 function solution(triangle) {
-    for(let i = 0; i < triangle.length-1; i++) {
-        const next = [...triangle[i+1]];
-        triangle[i].forEach((num, ii) => {
-            triangle[i+1][ii] = Math.max(triangle[i+1][ii], next[ii] + num);
-            triangle[i+1][ii+1] = Math.max(triangle[i+1][ii+1], next[ii+1] + num);
-        });
-    }
+    triangle.map((arr, i) => i > 0 && arr.map((_, ii) => triangle[i][ii] += Math.max(triangle[i-1][ii] || -Infinity, triangle[i-1][ii-1] || -Infinity)))
     return Math.max(...triangle[triangle.length-1]) 
 }
