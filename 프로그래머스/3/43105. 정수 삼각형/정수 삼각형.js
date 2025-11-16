34b65946ab7d9ccad34142b760659fe83b2f742c
sngmin <134246428+sngminn@users.noreply.github.com>
2025-11-16 22:25:06 +0900

    [level 3] Title: 정수 삼각형, Time: 7.43 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,16 +1,10 @@
 function solution(triangle) {
-    let summedTriangle = Array(triangle.length).fill()
-        .map((_, i) => Array(triangle[i].length).fill(0));
-    summedTriangle[0][0] = triangle[0][0];
-    
-    for(let col = 1; col < triangle.length; col++) {
-        for(let row = 0; row < triangle[col].length; row++) {
-            const num = triangle[col][row];
-            const prevNumL = summedTriangle[col-1][row-1] || 0;
-            const prevNumR = summedTriangle[col-1][row] || 0;
-            summedTriangle[col][row] = Math.max(prevNumL, prevNumR) + num;
-        } 
+    for(let i = 0; i < triangle.length-1; i++) {
+        const next = [...triangle[i+1]];
+        triangle[i].forEach((num, ii) => {
+            triangle[i+1][ii] = Math.max(triangle[i+1][ii], next[ii] + num);
+            triangle[i+1][ii+1] = Math.max(triangle[i+1][ii+1], next[ii+1] + num);
+        });
     }
-    
-    return Math.max(...summedTriangle[summedTriangle.length-1])
+    return Math.max(...triangle[triangle.length-1]) 
 }
