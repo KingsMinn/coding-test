60a5ea56927f8373e1acb0c72a22061cd0699310
sngmin <134246428+sngminn@users.noreply.github.com>
2025-11-21 23:54:28 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 19.98 ms, Memory: 39.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,26 +1,20 @@
 function solution(maps) {
-    const H = maps.length;
-    const W = maps[0].length;
-    
-    const DIRECTIONS = [[1, 0], [-1, 0], [0, 1], [0, -1]];
-    const queue = [[0, 0, 1]];
-    let head = 0;
+    const MOVEMENT = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+    const paths = [[1, 0, 0]];
     maps[0][0] = 0;
-    
-    while(head < queue.length) {
-        const [y, x, distance] = queue[head];
-        if (y === H - 1 && x === W - 1) { return distance };
+    for(let head = 0; head < paths.length; head++) {
+        const [dis, y, x] = paths[head];
         
-        DIRECTIONS.forEach(([dy, dx]) =>{
-            const targetY = dy + y;
-            const targetX = dx + x;
-            if (targetY >= 0 && targetY < H && targetX >= 0 && targetX < W && maps[targetY][targetX]) {
-                queue.push([targetY, targetX, distance + 1]);
-                maps[targetY][targetX] = 0;
+        for([my, mx] of MOVEMENT) {
+            if ((maps.length-1 === y) && (maps[0].length-1 === x)) {
+                return dis;
             }
-        });
-        head++;
-    }
-    
-    return -1
+            
+            if (maps[y + my] && maps[y + my][x + mx]) {
+                paths.push([dis+1, y + my, x + mx]);
+                maps[y + my][x + mx] = 0;
+            }
+        };
+    };
+    return -1;
 }
